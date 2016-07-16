2a92f38feacdff5f21298c99c976274204f82c9e
Nick Evans <nick@imin.co>
2016-07-16 17:56:22 +0100

    Updated Travis config

--- generator/deploy.sh
+++ generator/deploy.sh
@@ -51,7 +51,7 @@ echo ""
 echo "Checking if index.html has changed..."
 git add index.html
 git diff --cached --exit-code > /dev/null 2>&1
-if [ "$?" ]; then 
+if [ $? -ne 0 ]; then 
   echo "There are changes"; 
   echo ""
   echo "Committing index.html..."; 
