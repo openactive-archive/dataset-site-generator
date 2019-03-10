d0ac4da7e0a40ee22e4e14dfafa277980db17de9
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2019-03-10 12:03:16 +0000

    Update to accept redirect template file

--- generator/generator-standalone.js
+++ generator/generator-standalone.js
@@ -9,13 +9,14 @@ var chalk = require('chalk');
 var fs = require('fs');
 var generate = require('./generator.js').generate;
 
-function standalone(templateFile, stateFile, outputFile) {
+function standalone(templateFile, redirectTemplateFile, stateFile, outputFile) {
     var fs = require('fs');
 
-    var template = fs.readFileSync(templateFile, "utf8");
     var stateSerialised = fs.readFileSync(stateFile, "utf8");
     var state = JSON.parse(stateSerialised);
 
+    var template = fs.readFileSync(state.redirect ? redirectTemplateFile : templateFile, "utf8");
+    
     var output = generate(template, state);
 
     fs.writeFileSync(outputFile, output);
@@ -25,6 +26,7 @@ program
     .version('1.0.0')
     .usage('')
     .option('-t, --template [template.html file]', 'Template file containing placeholders')
+    .option('-r, --redirect [redirect_template.html file]', 'Redirect template file containing placeholders')
     .option('-m, --metadata [metadata.json file]', 'metadata.json file containing state')
     .option('-o, --output [index.html file]', 'File to write generated output')
     .parse(process.argv);
@@ -34,11 +36,12 @@ if(!program.template || !program.metadata || !program.output ) {
 } else {
     console.log("Generate params: ")
     console.log(" - template: " + program.template)
+    console.log(" - redirect: " + program.redirect)
     console.log(" - metadata: " + program.metadata)
     console.log(" - output: " + program.output)
 
     try {
-        standalone(program.template, program.metadata, program.output);
+        standalone(program.template, program.redirect, program.metadata, program.output);
         console.log(program.output + " updated successfully");
         process.exit(0);
     } catch (err) {
